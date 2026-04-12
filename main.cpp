8492952c486d117105bac747b732980fffc57897
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-04-12 16:00:56 +0200

    Struktura Sequence

--- main.cpp
+++ main.cpp
@@ -5,6 +5,18 @@
 
 using namespace std;
 
+struct Sequence{
+    public:
+        Sequence(
+            const char* name, std::uint32_t name_length, 
+            const char* data, std::uint32_t data_length,
+            const char* quaity, std::uint32_t quality_length
+        )
+        {
+            // TODO
+        }
+}
+
 int main() {
     MyClass obj("BioProject");
     obj.greet();
