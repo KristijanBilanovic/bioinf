573046ab0ff6001feaf216b3bffaa0343ff00888
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-04-15 21:11:10 +0200

    Removed test classes

--- src/MyClass.cpp
+++
@@ -1,8 +0,0 @@
-#include "MyClass.hpp"
-#include <iostream>
-
-MyClass::MyClass(const std::string& name) : name(name) {}
-
-void MyClass::greet() {
-    std::cout << "Hello from " << name << std::endl;
-}
